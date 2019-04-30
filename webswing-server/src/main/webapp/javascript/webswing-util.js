08430d77df219ce1a7560f8c0051f15118dc421b
Viktor Meszaros <viktor.meszaros@webswing.org>
2019-04-30 20:26:09 +0200

    resolve #20: webswingID cookie length

--- webswing-server/src/main/webapp/javascript/webswing-util.js
+++ webswing-server/src/main/webapp/javascript/webswing-util.js
@@ -101,9 +101,9 @@ define([ 'jquery' ], function Util($) {
     
     function GUID() {
         var S4 = function () {
-            return Math.floor(Math.random() * 0x10000).toString(16);
+            return Math.floor(Math.random() * 0x100000000).toString(16);
         };
-        return (S4() + S4() + S4());
+        return (S4() + S4() + S4() + S4());
     }
 
 });
